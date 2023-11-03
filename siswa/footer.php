f53b9ccaada48a524f2d9700042f8e9100da7e43
Dionisius Lucky <81998827+ZerfLich@users.noreply.github.com>
2023-11-03 08:38:17 +0700

    Delete siswa directory

--- siswa/footer.php
+++
@@ -1,5 +0,0 @@
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">UTS Semester 3 &copy; Keamanan Basis Data</div>
-                        </div>
-                    </div>
--- siswa/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-include "koneksi.php";
-	if (isset($_SESSION['username'])==''){
-		include "login.php";
-	}else{
-		include "konten.php";
-	}
-?>
